2972b9ad35d1cd02ad5134ed425a9e32e75be561
Xyzkrooo <deas.faras@gmail.com>
2024-10-03 16:17:19 +0700

    btn pdf

--- resources/views/data_master/guru/index.blade.php
+++ resources/views/data_master/guru/index.blade.php
@@ -7,9 +7,11 @@
                     <div class="card-header">
                         <div class="row">
                             <div class="col-12 d-flex justify-content-start">
-                                <a href="javascript:void(0)" class="btn btn-success mb-2 modalCreate" data-bs-toggle="modal"
-                                    data-bs-target="#create">+ Tambah</a>&nbsp; &nbsp;
+                                <a href="javascript:void(0)" class="btn btn-success mb-2 modalCreate" data-bs-toggle="modal"data-bs-target="#create">+ Tambah</a>
+                                &nbsp; &nbsp;
                                 <a href="javascript:void(0)" class="btn btn-success mb-2" id="export">Export Excel</a>
+                                &nbsp; &nbsp;
+                                <a href="javascript:void(0)" class="btn btn-danger mb-2" id="export">Printout Pdf</a>
                             </div>
                         </div>
                     </div>
