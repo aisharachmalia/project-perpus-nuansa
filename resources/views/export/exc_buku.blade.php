5a3decae182de1388414f67734a09f3cd5d91eae
BagasAp <bagasalvip07@gmail.com>
2024-10-03 15:30:07 +0700

    Export Excel Buku

---
+++ resources/views/export/exc_buku.blade.php
@@ -0,0 +1,29 @@
+<table>
+    <tr>
+        <th colspan="8" style="text-align: center; color: blue">Rekap Buku</th>
+    </tr>
+</table>
+<table border="1">
+    <tr>
+        <th>No</th>
+        <th>Judul Buku</th>
+        <th>ISBN</th>
+        <th>Nama Penulis</th>
+        <th>Nama Penerbit</th>
+        <th>Kategori</th>
+        <th>Mata Pelajaran</th>
+        <th>Tahun Terbit</th>
+    </tr>
+    @foreach ($buku as $key => $item)
+        <tr>
+            <td>{{ $key+1 }}</td>
+            <td>{{ $item->dbuku_judul }}</td>
+            <td>{{ $item->dbuku_isbn }}</td>
+            <td>{{ $item->dpenulis_nama_penulis }}</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td>{{ $item->dbuku_thn_terbit }}</td>
+        </tr>
+    @endforeach
+</table>
