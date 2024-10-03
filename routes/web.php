b862fcadadbe258827dfff30c61072764cc6303c
BagasAp <bagasalvip07@gmail.com>
2024-10-03 11:22:32 +0700

    web

--- routes/web.php
+++ routes/web.php
@@ -59,7 +59,7 @@ Route::put('/kelas-update/{id?}', [App\Http\Controllers\KelasController::class, 
 Route::prefix('data-master')->group(function () {
     //Guru
     Route::get('/guru', [App\Http\Controllers\GuruController::class, 'pageGuru'])->name('data_master.guru');
-    Route::post('/pustakawan/add', [App\Http\Controllers\GuruController::class, 'addGuru'])->name('data_master.guru.add');
+    Route::post('/guru/add', [App\Http\Controllers\GuruController::class, 'addGuru'])->name('data_master.guru.add');
     Route::get('/guru/show/{id}', [App\Http\Controllers\GuruController::class, 'showGuru'])->name('data_master.guru.show');
     Route::put('/guru/edit/{id}', [App\Http\Controllers\GuruController::class, 'editGuru'])->name('data_master.guru.edit');
     Route::delete('/guru/delete/{id}', [App\Http\Controllers\GuruController::class, 'deleteGuru'])->name('data_master.guru.delete');
