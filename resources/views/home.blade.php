4e58cfc836cdfb3cf66430a143a6db7acc70a1a0
aisharachma <sharachmall@gmail.com>
2024-10-15 09:31:06 +0700

    dashboard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -149,7 +149,7 @@
                                     </div>
                                     <div class="col-md-8">
                                         <h6 class="text-muted font-semibold">Peminjaman</h6>
-                                        <h6 class="font-extrabold mb-0">80.000</h6>
+                                        <h6 class="font-extrabold mb-0">80</h6>
                                     </div>
                                 </div>
                             </div>
